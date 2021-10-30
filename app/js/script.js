1c79309eae0298e7539c06e2f6f75ac99acff387
David-bit986 <tanasescudavid@gmail.com>
2021-10-30 09:40:32 +0300

    save 2

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hellow")
+const burger = document.getElementById('burger');
+const nav = document.getElementById('nav-links');
+
+burger.addEventListener('click', function(){
+    if(nav.classList.contains('links-toggle')){
+        nav.classList.toggle('nav-active')
+        burger.classList.toggle('rotate')
+    }
+})
+
+
