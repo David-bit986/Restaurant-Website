60fa591d5bbf093030972bc6616bb43a17c21fae
David-bit986 <tanasescudavid@gmail.com>
2021-11-20 10:38:09 +0200

    final save main page

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,65 @@
 const burger = document.getElementById('burger');
 const nav = document.getElementById('nav-links');
-var meals = document.querySelector('.dsadas');
+const commentsrc = document.querySelector('.t-coment')
+const imagsrc = document.getElementById('t-imgs');
+const namesrc = document.querySelector('.t-name');
+const leftArrow = document.getElementById('r-left');
+const rightArrow = document.getElementById('r-right')
+const allCircle = document.querySelectorAll('button.p-circle')
+
+var i = 0
+var images = [
+    {
+        'img': 'dist/images/Ellipse 21.jpg',
+        'name': 'Ruby',
+        'recomandation':'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts',
+    },
+    {
+        'img': 'dist/images/Ellipse 33.jpg',
+        'name': 'David',
+        'recomandation':'Far far away, behind the word mountains, far from td Consonantia, there live the blind texts',
+    },
+    {
+        'img': 'dist/images/Ellipse 34.jpg',
+        'name': 'Herbert',
+        'recomandation':'Far far away, behind the word mountains, far from the countri live the blind texts',
+    },
+    {
+        'img': 'dist/images/Ellipse 35.jpg',
+        'name': 'Alvin',
+        'recomandation':'Far rom the countries Vokalia and Consonantia, there live the blind texts',
+    }
+]
+
+
+function changeImg(){
+
+    rightArrow.addEventListener("click", function(){
+        checkifhasclass()
+        if (i > 2){
+            i = -1
+        }
+        i = i + 1;
+        imagsrc.src = images[i]['img'];
+        namesrc.innerHTML = images[i]['name'];
+        commentsrc.innerHTML = images[i]['recomandation'];
+        checkifhasclass()
+    });
+
+    leftArrow.addEventListener("click", function(){
+        checkifhasclass()
+        if(i < 1){
+            i = 4;
+        }
+        i = i - 1;
+        imagsrc.src = images[i]['img'];
+        namesrc.innerHTML = images[i]['name'];
+        commentsrc.innerHTML = images[i]['recomandation'];
+        checkifhasclass()
+    });
+}
+
+window.onload = changeImg
 
 burger.addEventListener('click', function(){
     if(nav.classList.contains('links-toggle')){
@@ -9,4 +68,10 @@ burger.addEventListener('click', function(){
     }
 })
 
-
+function checkifhasclass(){
+    if(allCircle[i].classList.contains('active')){
+        allCircle[i].classList.remove("active");
+    }else{
+        allCircle[i].classList.add("active");
+    }
+}
